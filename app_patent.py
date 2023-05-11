b7b73f91ef33f214a2695311e086abf0323f4510
Kazuya Saisho <60130512+saichon79@users.noreply.github.com>
2023-05-12 02:28:57 +0900

    Update app_patent.py

--- app_patent.py
+++ app_patent.py
@@ -44,7 +44,7 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
+st.title("Patent AI Assistant")
 st.write("ChatGPT APIを使ったチャットボットです。")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
